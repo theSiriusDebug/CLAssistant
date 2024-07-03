d999fd8a142a0abad6b9ad8203e297cabbf90c75
theSiriusDebug <misha.ch2006@gmail.com>
2024-07-03 20:28:52 +0200

    Initial commit

---
+++ src/main/java/main/assistant/ClAssistantApplication.java
@@ -0,0 +1,13 @@
+package main.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/assistant/ClAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package main.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
